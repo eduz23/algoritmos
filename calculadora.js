2b6994ccef4f615d2712b65e4e8f9f1b47dccae6
Luiz Eduardo <10edu2008@gmail.com>
2024-09-17 21:16:05 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,26 +1,32 @@
 const prompt = require("prompt-sync")()
-const num1 = prompt("Selecione o primeiro numero: ")
-const num2 = prompt("Selecione o segundo numero: ")
-const conta = prompt("Selecione a operação mátematica que deseja realizar: ")
+
+function calcularoperacao(){
+let num1 = prompt("Selecione o primeiro numero: ")
+let num2 = prompt("Selecione o segundo numero: ")
+let conta = prompt("Selecione a operação mátematica que deseja realizar: ")
 switch (conta) {
     case "soma":
-        const soma = Number(num1) + Number(num2)
+        let soma = Number(num1) + Number(num2)
         console.log("A soma foi ", soma)
         break;
     case "subtracao":
-        const subtracao = (num1) - (num2)
+        let subtracao = (num1) - (num2)
         console.log("A subtração foi ", subtracao)
         break;
     case "multiplicacao":
-        const multi = (num1) * (num2)
+        let multi = (num1) * (num2)
         console.log("A multiplicação foi ", multi)
         break;
     case "divisao":
         if (num2!=0){
-        const div = (num1) / (num2)
+        let div = (num1) / (num2)
         console.log("A divisão deu ", div)}
         else {console.log("Erro, o 0 é inválido")}
         break;
     default:
-        console.log("ERRO! Operação matematica invalida")
-}
+        console.log("ERRO! Operação matematica invalida, responda novamente")
+        calcularoperacao()
+        break;
+    }
+}   
+calcularoperacao()
